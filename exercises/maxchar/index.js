0679fedb70535756e34c7fcebed948aab1aa2fc2
searchmoon <moon901212@gmail.com>
2024-06-03 23:57:45 +0900

    algorithms -section6

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,37 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// function maxChar(str) {
+//   const chars = {};
+
+//   for (let char of str) {
+//     chars[char] = chars[char] + 1 || 1;
+//   }
+
+//   return;
+// }
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    if (!charMap[char]) {
+      charMap[char] = 1;
+    } else {
+      charMap[char]++;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
