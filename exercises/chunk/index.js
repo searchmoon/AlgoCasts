8b962d76a779b987204dbfb85623a34cdaad5820
searchmoon <moon901212@gmail.com>
2024-06-04 23:46:25 +0900

    algorithms -section8

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,46 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// 풀이 1.
+function chunk(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+
+  return chunked;
+}
+
+// 풀이 2. for 문 사용
+function chunk(array, size) {
+  const newArr = [];
+
+  for (let i = 0; i < array.length; i += size) {
+    let slice = array.slice(i, i + size);
+    newArr.push(slice);
+  }
+
+  return newArr;
+}
+
+// 풀이 3. while문 사용
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,16 +5,6 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-// function maxChar(str) {
-//   const chars = {};
-
-//   for (let char of str) {
-//     chars[char] = chars[char] + 1 || 1;
-//   }
-
-//   return;
-// }
-
 function maxChar(str) {
   const charMap = {};
   let max = 0;
