ef3492a38cbcb6d1e425e9f574d56ab8b5cb622b
searchmoon <moon901212@gmail.com>
2024-06-08 01:42:35 +0900

    algorithms -section12

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,27 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+
+  for (let row = 0; row < n; row++) {
+    let level = "";
+
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += "#";
+      } else {
+        level += " ";
+      }
+    }
+
+    console.log(level);
+  }
+}
+// (n - 1) * 2 + 1 하면 column
+// row 는 n
+// 그만큼 반복.
+// 이중 for문인데, 컬럼에서는
+// n-1까지는
 
 module.exports = pyramid;
