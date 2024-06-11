80907f290397a0deea3528445cbf83fe02ffe699
searchmoon <moon901212@gmail.com>
2024-06-11 23:45:49 +0900

    algorithms -section20

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,46 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+// 2개의 stack 으로, queue를 만들기
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    this.second = new Stack();
+  }
+
+  add(record) {
+    this.first.push(record);
+  }
+
+  remove() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+
+    const record = this.second.pop();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record;
+  }
+
+  peek() {
+    while (this.first.peek()) {
+        this.second.push(this.first.pop());
+      }
+  
+      const record = this.second.peek();
+  
+      while (this.second.peek()) {
+        this.first.push(this.second.pop());
+      }
+  
+      return record;
+  }
+}
 
 module.exports = Queue;
