a06d7230941d90082a92b989e4241ebd60aecd11
searchmoon <moon901212@gmail.com>
2024-06-12 23:56:23 +0900

    algorithms -section31, 32 정렬 알고리즘

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,20 +1,42 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
+//버블 정렬
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
 }
 
+//선택 정렬
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
 
+    if (indexOfMin !== i) {
+      let lessor = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lessor;
+    }
+  }
+  return arr;
 }
 
-function mergeSort(arr) {
+//병합 정렬
+function mergeSort(arr) {}
 
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
